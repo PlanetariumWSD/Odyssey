bfd1bf95188552d9e22cbc0b6cb93b3e281e3f42
TheRealPiGuy <20naacker@wausauschools.org>
2019-06-05 15:20:59 -0500

    Test

--- scripts/scenario_alpha.lua
+++ scripts/scenario_alpha.lua
@@ -3,6 +3,8 @@
 --- You must check out ship systems and complete an initial mission.
 -- Type: Mission
 
+--My name is Nathanael Acker.
+
 --[[Problems
 no rep at start....
 Unclear who to contact for first mission
